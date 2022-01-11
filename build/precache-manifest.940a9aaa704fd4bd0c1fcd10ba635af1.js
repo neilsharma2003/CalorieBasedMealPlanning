ac804aadc7b3e8facfc56f97dbf9b97ed1ab0b6c
Neil Sharma <neilsharma@Neils-MacBook-Air-2.local>
2022-01-11 18:21:04 -0500

    initial-deploy

---
+++ build/precache-manifest.940a9aaa704fd4bd0c1fcd10ba635af1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c7a10e7e0d183805db6bf2feda22a11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "80d1e1c565702ac5e976",
+    "url": "/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "63ea4381f5056a9fcc7c",
+    "url": "/static/css/main.18cde0c4.chunk.css"
+  },
+  {
+    "revision": "80d1e1c565702ac5e976",
+    "url": "/static/js/2.cf532ea2.chunk.js"
+  },
+  {
+    "revision": "ded63d2a4892df9c1109",
+    "url": "/static/js/3.cdb3f5bb.chunk.js"
+  },
+  {
+    "revision": "63ea4381f5056a9fcc7c",
+    "url": "/static/js/main.52b1c296.chunk.js"
+  },
+  {
+    "revision": "87e6b7d9f9dcf20603b4",
+    "url": "/static/js/runtime~main.f8593695.js"
+  }
+]);
